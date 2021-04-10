1d9b51bb70b481640fdd0faf076d095a5511ce0f
Shtonks <58079793+Shtonks@users.noreply.github.com>
2021-04-10 21:12:32 +0100

    Update Trap.h

--- Trap.h
+++ Trap.h
@@ -22,13 +22,14 @@ class Trap : public Item, public RiskFactor {
         bool vulnerable;
         bool resistant;
     public:
-        Trap(const string &name = "", const string &description = "", int timeMod = 0, int dmgType = NOTHING,int baseDmg = 0, int maxDmg = 0);
+        Trap(const string &name = "", const string &description = "", int timeMod = 0, int dmgType = NOTHING,int baseDmg = 0, int maxDmg = 1);
         string getDmgType();
         string getDmgType(int typeNum);
         void setDmgType(int dmgtype);
         void setDealtDmg(); //<ISHA> calculates damage dealt between baseDamage - maxDamage
         void setDealtDmg(double factor); //<ISHA> Multiplies Dealt damage by a factor depending on room
         int getDealtDmg();
+        int getMaxDmg();
         void setVulnerable(bool vulnerable);
         void setResistant(bool resistant);
         string toString();
